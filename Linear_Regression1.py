d97a56eeff56cfc6da33db24f1c52c6ef360003a
Aashutosh Nangare <aashutoshnangare@gmail.com>
2026-09-03 18:51:02 +0530

    Add Linear_Regression1.py

---
+++ Linear_Regression1.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def MarvellousPredictor():
+    #Load the Data
+    X = [1,2,3,4,5]
+    Y = [3,4,2,4,5]
+
+    print("Values of Independent Variables : X",X)
+    print("Values of Dependent Variables : Y",Y)\
+    
+    mean_X = np.mean(X)
+    mean_Y = np.mean(Y)
+
+    print("X _MEAN is :",mean_X)
+    print("Y_MEAN is : ",mean_Y)
+
+    
+
+def main():
+    MarvellousPredictor()
+
+if __name__ == "__main__":
+    main() 
