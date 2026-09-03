037ebc9a5536f44a291c46ac293177da6b6d8325
Aashutosh Nangare <aashutoshnangare@gmail.com>
2026-09-03 18:51:54 +0530

    Add BoostingRegression_CaliforniaHousing.py

---
+++ BoostingRegression_CaliforniaHousing.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_squared_error,r2_score
+
+#---------------------------------------------------------
+#Step 1 : Load the Dataset
+#---------------------------------------------------------
+
+df = pd.read_csv("california_housing.csv")
+print("Shape of dataset : ",df.shape)
+print("First 5 records : ",df.head())
+
+#---------------------------------------------------------
+#Step 2 : Seperate Features and Labels 
+#---------------------------------------------------------
+
+X = df.drop("target",axis=1)
+Y = df["target"]
+
+#---------------------------------------------------------
+#Step 3 : Split datset for training and testing 
+#---------------------------------------------------------
+
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,random_state=42)
+
+#---------------------------------------------------------
+#Step 4 : Create Graident boosting model
+#---------------------------------------------------------
+
+boost_model = GradientBoostingRegressor(
+    n_estimators=100,
+    learning_rate=0.1,
+    max_depth=3,
+    random_state=42
+    )
+
+#---------------------------------------------------------
+#Step 5 : Train boosting model
+#---------------------------------------------------------
+
+boost_model.fit(X_train,Y_train)
+
+#---------------------------------------------------------
+#Step 6 : Test boosting model
+#---------------------------------------------------------
+
+Y_pred = boost_model.predict(X_test)
+
+#---------------------------------------------------------
+#Step 7 : Evaluate boosting model
+#---------------------------------------------------------
+
+print("MeanSquaredError : ",mean_squared_error(Y_test,Y_pred))
+print("R Square : ",r2_score(Y_test,Y_pred))
+
+
